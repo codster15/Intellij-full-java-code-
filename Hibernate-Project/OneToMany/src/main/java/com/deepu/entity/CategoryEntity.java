35a098a77c998b9b755a69b52f0b1ba41480cd34
Codster dev <deepaksingh.desire@gmail.com>
2024-03-23 16:59:16 +0530

    test

--- Hibernate-Project/OneToMany/src/main/java/com/deepu/entity/CategoryEntity.java
+++ Hibernate-Project/OneToMany/src/main/java/com/deepu/entity/CategoryEntity.java
@@ -1,4 +1,20 @@
 package com.deepu.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table (name = "tbl_category")
 public class CategoryEntity {
+
+    @Id
+    @Column(name = "Category_id")
+    Integer categoryID;
+
+
+
+
+
 }
