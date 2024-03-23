60c8277b7024434ba9f6c4d47176aa4dbe080ac9
Codster dev <deepaksingh.desire@gmail.com>
2024-03-23 17:25:50 +0530

    test

--- Hibernate-Project/OneToMany/src/main/java/com/deepu/entity/CategoryEntity.java
+++ Hibernate-Project/OneToMany/src/main/java/com/deepu/entity/CategoryEntity.java
@@ -1,20 +1,59 @@
 package com.deepu.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
-@Table (name = "tbl_category")
+@Table (name = "Tbl_Category")
 public class CategoryEntity {
 
     @Id
     @Column(name = "Category_id")
     Integer categoryID;
 
+    @Column (name = "Category_Name")
+    String CategoryName ;
 
 
+    @OneToMany( cascade = CascadeType.ALL)
+       @JoinColumn (name = "Category_id_fk")
+
+    List<ProductEntity> lstOfProduct ;
+//------------------------------------------------------------------------------------------------------------------
+    public Integer categoryID() {
+        return categoryID;
+    }
+
+    public CategoryEntity setCategoryID(Integer categoryID) {
+        this.categoryID = categoryID;
+        return this;
+    }
+
+    public String CategoryName() {
+        return CategoryName;
+    }
+
+    public CategoryEntity setCategoryName(String categoryName) {
+        CategoryName = categoryName;
+        return this;
+    }
+
+    public List<ProductEntity> lstOfProduct() {
+        return lstOfProduct;
+    }
+
+    public CategoryEntity setLstOfProduct(List<ProductEntity> lstOfProduct) {
+        this.lstOfProduct = lstOfProduct;
+        return this;
+    }
 
 
+    @Override
+    public String toString() {
+        return "CategoryEntity{" +
+                "categoryID=" + categoryID +
+                ", CategoryName='" + CategoryName + '\'' +
+                ", lstOfProduct=" + lstOfProduct +
+                '}';
+    }
 }
--- Hibernate-Project/OneToMany/src/main/java/com/deepu/entity/ProductEntity.java
+++ Hibernate-Project/OneToMany/src/main/java/com/deepu/entity/ProductEntity.java
@@ -1,5 +1,59 @@
 package com.deepu.entity;
 
+import javax.persistence.*;
+import java.util.Arrays;
+
+@Entity
+@Table (name = "Tbl_Product")
 public class ProductEntity {
+
+    @Id
+    @Column(name = "Product_ID")
+    private Integer ProductId;
+
+    @Column(name = "Product_name")
+    private String ProductName ;
+
+    @Lob
+     @Column(name = "Product_Image")
+    byte[] ProductImg;
+
+    //-----------------------------------------------------------------------------------
+    public Integer ProductId() {
+        return ProductId;
+    }
+
+    public ProductEntity setProductId(Integer productId) {
+        ProductId = productId;
+        return this;
+    }
+
+    public String ProductName() {
+        return ProductName;
+    }
+
+    public ProductEntity setProductName(String productName) {
+        ProductName = productName;
+        return this;
+    }
+
+    public byte[] ProductImg() {
+        return ProductImg;
+    }
+
+    public ProductEntity setProductImg(byte[] productImg) {
+        ProductImg = productImg;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductEntity{" +
+                "ProductId=" + ProductId +
+                ", ProductName='" + ProductName + '\'' +
+                ", ProductImg=" + Arrays.toString(ProductImg) +
+                '}';
+    }
+
 
 }
