19233cdc0a9045cdec2f2758fb35bf4da423b64a
Codster dev <deepaksingh.desire@gmail.com>
2024-04-02 01:17:07 +0530

    add

--- Hibernate-Project/HibernateApplication1/src/main/java/com/deepakIT/StudentDAO/StudentDAO.java
+++
@@ -1,7 +0,0 @@
-package com.deepakIT.StudentDAO;
-import com.deepakIT.entity.Student;
-
-
-public interface StudentDAO {
-    void saveStudent(Student student);
-}
--- Hibernate-Project/JpqlProject/src/main/java/com/deepu/SqLQuery/SqlQuery.java
+++
@@ -1,7 +0,0 @@
-package com.deepu.SqLQuery;
-
-public class SqlQuery {
-    public static final String QueryByID = "from EmployeeEntity e where e.id = ?1";
-    public static final String Queryemp = "from EmployeeEntity e ";
-    public static final String QueryByNameSal = "select e.name , e.salary from EmployeeEntity e ";
-}
